760fca11c7d219f7ef0ac4a0e60e3e0f68f8fd02
Philipp Bischoff <philippbischoff@Philipps-MacBook-Pro.local>
2019-07-15 11:20:34 +0200

    added useful links as comments

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ async function request() {
 
   const result = await fetch(url, {
     method: "GET",
+    // in mode no-cors the response object will always have status: 0, ok: false and message: '', https://stackoverflow.com/questions/40182785/why-fetch-return-a-response-with-status-0
     mode: "no-cors",
     headers: {
       "Content-Type": "application/json"
--- server/index.php
+++ server/index.php
@@ -1,5 +1,7 @@
 <?php
 
+// example: https://gist.github.com/james2doyle/33794328675a6c88edd6
+
 //echo "hello world";
 header('Content-Type: application/json');
 http_response_code(200);
